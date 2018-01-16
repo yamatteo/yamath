35b66c2cd3bfce9432f4fa2852d6e2068bca8b60
Your Name <you@example.com>
2018-01-16 17:02:52 +0100

    Amend

--- config.py
+++
@@ -1,9 +0,0 @@
-production = False
-testing = True
-database = 'yamath'
-from mongoengine import connect
-
-if production:
-    connect(host="mongodb://admin:ichigoichie@cluster0-shard-00-00-txgpn.mongodb.net:27017,cluster0-shard-00-01-txgpn.mongodb.net:27017,cluster0-shard-00-02-txgpn.mongodb.net:27017/%s?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin" % database)
-else:
-    connect(database)
