1c28709740cdf4d3f393c35040e6297954963816
yamatteo <yamatteo@gmail.com>
2018-03-01 16:15:03 +0100

    Automatic sync from Panda

--- __init__.py
+++ __init__.py
@@ -3,6 +3,10 @@ import base64
 import hashlib
 import os
 import sys
+from dotenv import load_dotenv
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+# from pprint import pprint
+# pprint(dict(os.environ))
 # Pare non ci sia più bisogno di tutti questi aggiustamenti, almeno fino al prossimo cambio di contesto
 # try:
 #     sys.path.append(__path__[0])
@@ -11,6 +15,7 @@ import sys
 #     pass
 
 from flask import Flask, render_template
+from flask_admin import Admin
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from flask_cors import CORS
 from mongoengine import connect
@@ -20,10 +25,23 @@ app = Flask(__name__)
 # Essendo solo un api, l'applicazione non ha più bisogno di static
 json = FlaskJSON(app)
 cors = CORS(app)
-fasthash_dictionary = {"admin":"0="}
+admin = Admin(app, name='yamath', template_mode='bootstrap3')
+from models import *
+from flask_admin.contrib.mongoengine import ModelView
+admin.add_view(ModelView(User, 'User'))
+admin.add_view(ModelView(Node, 'Node'))
+admin.add_view(ModelView(Question, 'Question'))
+admin.add_view(ModelView(Profile, 'Profile'))
+
+
+fasthash_dictionary = {"admin":"0"}
+try:
+    db_client = connect(host=os.environ['MONGODB_URI'])
+except KeyError:
+    db_client = connect('testing')
 # app.config["RANDOM_SALT"] = base64.b64encode(os.urandom(20))
 # db_client = connect(host="mongodb://admin:ichigoichie@cluster0-shard-00-00-txgpn.mongodb.net:27017,cluster0-shard-00-01-txgpn.mongodb.net:27017,cluster0-shard-00-02-txgpn.mongodb.net:27017/testing?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
-from models import db_client
+# from models import db_client
 
 # @app.route('/')
 # def index():
