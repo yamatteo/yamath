391496bcc792e2c2c4ef98e34b832b24b7a2793b
Matteo Bortolotto <yamatteo@gmail.com>
2018-03-14 11:50:40 +0000

    Some changes

--- __init__.py
+++ __init__.py
@@ -43,9 +43,9 @@ except KeyError:
 # db_client = connect(host="mongodb://admin:ichigoichie@cluster0-shard-00-00-txgpn.mongodb.net:27017,cluster0-shard-00-01-txgpn.mongodb.net:27017,cluster0-shard-00-02-txgpn.mongodb.net:27017/testing?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
 # from models import db_client
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 from api import *
 
