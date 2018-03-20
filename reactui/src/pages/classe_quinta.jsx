32402d9f5506fee2a4aeedf37b2fba3ca72d923b
yamatteo <yamatteo@gmail.com>
2018-03-20 17:29:32 +0100

    Automatic sync from Panda

--- reactui/src/pages/classe_quinta.jsx
+++ reactui/src/pages/classe_quinta.jsx
@@ -18,6 +18,25 @@ export function ClasseQuinta(props) {
             <li>{'$y = \\sqrt[3]{3x + 8} - \\sqrt{4x^2 + x - 3}$'}</li>
             <li>{'$y = \\log_4({3x + 2}) + \\sqrt{5 - x + \\frac14}$'}</li>
           </ol>
+          <h3>Proprietà delle funzioni</h3>
+          <p>Ricordando le definizioni di funzione <em>pari</em>, <em>dispari</em>, <em>iniettiva</em>, <em>suriettiva</em>, <em>crescente</em>, <em>decrescente</em>, <em>periodica</em> e <em>limitata</em>, stabilisci dal grafico e analiticamente (quando puoi) quali caratteristiche hanno le seguenti funzioni.</p>
+          <ol type='a'>
+            <h5>Funzioni polinomiali</h5>
+            <li>{'$y = 3x + 4$'}</li>
+            <li>{'$y = 9x^2 - 16$'}</li>
+            <li>{'$y = \\frac14x^3 - \\frac34x$'}</li>
+            <li>{'$y = -x^4 + 2x^3 - 5$'}</li>
+            <h5>Funzioni polinomiali fratte</h5>
+            <li>{'$y = \\frac{1}{x - 1}$'}</li>
+            <li>{'$y= \\frac{x^2+2}{x^2-2}$'}</li>
+            <li>{'$y = 3 + \\frac{1}{x-3} - \\frac{2}{x+5}$'}</li>
+            <li>{'$y = \\frac{1}{1 + \\frac{1}{x}}$'}</li>
+            <h5>Funzioni irrazionali</h5>
+            <li>{'$y = \\sqrt{x}$'}</li>
+            <li>{'$y = \\sqrt{x^2 - 4}$'}</li>
+            <li>{'$y = \\sqrt[3]{x^5}$'}</li>
+            <li>{'$y = \\frac{1}{\\sqrt{x + 4}}$'}</li>
+          </ol>
         </div>
       </div>
     </div>
